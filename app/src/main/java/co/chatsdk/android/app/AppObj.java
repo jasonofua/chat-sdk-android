6e8d020f2b0b8b5f4abb2eb8e104c4373de7b2a0
Conrad Martin <conrad.martin.1@gmx.de>
2018-11-01 14:34:05 +0100

    this is the new branch

--- app/src/main/java/co/chatsdk/android/app/AppObj.java
+++ app/src/main/java/co/chatsdk/android/app/AppObj.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.support.multidex.MultiDex;
 import android.support.multidex.MultiDexApplication;
 
+import java.util.HashMap;
+
 import co.chatsdk.core.error.ChatSDKException;
 import co.chatsdk.core.session.ChatSDK;
 import co.chatsdk.core.session.Configuration;
@@ -42,6 +44,73 @@ public class AppObj extends MultiDexApplication {
         FirebasePushModule.activate();
 //        FirebaseUIModule.activate(context, EmailAuthProvider.PROVIDER_ID, PhoneAuthProvider.PROVIDER_ID);
 
+        testHash();
+
+    }
+
+    void testHash () {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("name", "Ben");
+        map.put("pet", "cat");
+        map.put("address/number", "125");
+        map.put("address/city", "Berlin");
+        map.put("address/abode/room", "1");
+
+        HashMap<String, Object> mapA = new HashMap<>();
+        HashMap<String, Object> mapB = new HashMap<>();
+        HashMap<String, Object> mapC = new HashMap<>();
+
+        mapC.put("room", "1");
+
+        mapB.put("number", "125");
+        mapB.put("city", "Berlin");
+        mapB.put("abode", mapC);
+
+        mapA.put("name", "Ben");
+        mapA.put("pet", "cat");
+        mapA.put("address", mapB);
+
+        HashMap<String, String> test1 = flatten(mapA);
+        HashMap<String, Object> test2 = expand(map);
+
+        // Recursive functions
+
+    }
+
+    public HashMap<String, String> flatten (HashMap<String, Object> map) {
+        HashMap <String, String> flattenedMap = null;
+
+        for (String key : map.keySet()) {
+            Object value = map.get(key);
+            if (value instanceof HashMap) {
+                HashMap<String, Object> childHash = (HashMap<String, Object>) value;
+                for (String childKey : childHash.keySet()) {
+                    String compoundKey = key + "/" + childKey;
+                    Object childValue = childHash.get(childKey);
+                    if (childValue instanceof HashMap) {
+                        HashMap<String, String> childChildHash = (HashMap<String, String>) childValue;
+                        for (String childChildKey : childChildHash.keySet()) {
+                            String compoundCompoundKey = compoundKey + "/" + childChildKey;
+                            String childChildValue = childChildHash.get(childChildKey);
+                            flattenedMap.put(compoundCompoundKey, childChildValue);
+                        }
+                    }
+                    else {
+                        String stringChildValue = childValue.toString();
+                        flattenedMap.put(compoundKey, stringChildValue);
+                    }
+                }
+            }
+            else {
+                String stringValue = value.toString();
+                flattenedMap.put(key, stringValue);
+            }
+        }
+        return flattenedMap;
+    }
+
+    public HashMap<String, Object> expand (HashMap<String, String> map) {
+        return null;
     }
 
     @Override
